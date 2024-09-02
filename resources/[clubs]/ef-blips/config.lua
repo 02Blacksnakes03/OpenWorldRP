e6f6da6c5193af50eaab43ea5d19d01ac414a8ed
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-03 01:42:46 +0200

    Setting Up

---
+++ resources/[clubs]/ef-blips/config.lua
@@ -0,0 +1,53 @@
+Config = Config or {}
+
+---EF-Blips  
+Config.blipsShow = true
+
+Config.Locations = {
+[1] = {
+    vector = vector3(113.4708, -1286.9852, 28.2601), 
+    text = "Vanilla Unicorn", 
+    color = 48, 
+    sprite = 614, 
+    scale = 0.7,
+},
+[2] = {
+    vector = vector3(748.8309, -558.2845, 33.6427), 
+    text = "Medusa disco", 
+    color = 1, 
+    sprite = 614, 
+    scale = 0.7,
+},
+[3] = {
+    vector = vector3(369.7126, 247.3465, 103.0062),
+    text = "Neko Club", 
+    color = 26, 
+    sprite = 614, 
+    scale = 0.7,
+},
+[4] = {
+    vector = vector3(201.4832, -3166.9233, 5.8145),
+    text = "Club77", 
+    color = 27, 
+    sprite = 614, 
+    scale = 0.7,
+},
+[5] = {
+    vector = vector3(388.1999, -753.0205, 29.5208),
+    text = "LS Club", 
+    color = 46, 
+    sprite = 614, 
+    scale = 0.7,
+},
+-- Example 
+-- [[
+-- [4] = {
+--     vector = vector3(0, 0, 0),
+--     text = "Blip Name", 
+--     color = 3, 
+--     sprite = Blip ID, 
+--     scale = size,
+-- }, ]]
+
+--- add more
+}
---
+++ resources/[clubs]/myDj/config.lua
@@ -0,0 +1,40 @@
+Config = {}
+Translation = {}
+
+Translation = {
+    ['de'] = {
+        ['DJ_interact'] = 'Drücke ~g~E~s~, um auf das DJ Pult zuzugreifen',
+        ['title_does_not_exist'] = '~r~Dieser Titel existiert nicht!',
+    },
+
+    ['en'] = {
+        ['DJ_interact'] = 'Press ~g~E~s~, to access the DJ desk',
+        ['title_does_not_exist'] = '~r~This title does not exist!',
+    }
+}
+
+Config.Locale = 'en'
+
+Config.useESX = false -- can not be disabled without changing the callbacks
+Config.enableCommand = false
+
+Config.enableMarker = true -- purple marker at the DJ stations
+
+Config.DJPositions = {
+    {
+        name = 'vanilla',
+        pos = vector3(120.5567, -1281.3678, 29.4805),
+        requiredJob = nil, 
+        range = 25.0, 
+        volume = 1.0 --[[ do not touch the volume! --]]
+    },
+    {
+        name = 'medusa',
+        pos = vector3(755.7354, -554.6202, 29.3346),
+        requiredJob = nil, 
+        range = 25.0, 
+        volume = 1.0 --[[ do not touch the volume! --]]
+    }
+
+    --{name = 'bahama', pos = vector3(-1381.01, -616.17, 31.5), requiredJob = 'DJ', range = 25.0}
+}
