09060eb2d6b48adc54c08fd75e16885d14c26846
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-03 01:57:25 +0200

    Setting Up

---
+++ resources/[streams]/int_digitalden/__resource.lua
@@ -0,0 +1,6 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+Discord_Mapping 'https://discord.gg/NCHub'
+YouTube 'https://www.youtube.com/@NCHub'
+Discord_Tag 'NaorNC'
---
+++ resources/[streams]/tgm_legion/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+files {
+	'downtown.ymt',
+    'pillbox_hill.ymt'
+}
+
+data_file 'SCENARIO_INFO_FILE' 'downtown.ymt'
+data_file 'SCENARIO_POINTS_PSO_FILE' 'downtown.ymt'
+data_file 'SCENARIO_INFO_FILE' 'pillbox_hill.ymt'
+data_file 'SCENARIO_POINTS_PSO_FILE' 'pillbox_hill.ymt'
+
