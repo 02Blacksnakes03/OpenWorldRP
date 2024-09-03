7ab19a126611dff89d3f3aa1b8e9bbb4b36432b4
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-03 18:09:46 +0200

    Translation DE

--- resources/[garageandrent]/np-garage/locales/en.lua
+++ resources/[garageandrent]/np-garage/locales/en.lua
@@ -1,32 +1,32 @@
 local Translations = {
     error = {
-        no_vehicles = 'There are no vehicles in this location!',
-        not_depot = 'Your vehicle is not in depot',
-        not_owned = 'This vehicle can\'t be stored',
-        not_correct_type = 'You can\'t store this type of vehicle here',
-        not_enough = 'Not enough money',
-        no_garage = 'None',
-        vehicle_occupied = 'You can\'t store this vehicle as it is not empty',
-        vehicle_not_tracked = 'Could not track vehicle',
-        no_spawn = 'Area too crowded'
+        no_vehicles = 'Es sind keine Fahrzeuge an diesem Ort vorhanden!',
+        not_depot = 'Ihr Fahrzeug befindet sich nicht im Depot',
+        not_owned = 'Dieses Fahrzeug kann nicht gespeichert werden',
+        not_correct_type = 'Sie können diesen Fahrzeugtyp hier nicht speichern',
+        not_enough = 'Nicht genug Geld',
+        no_garage = 'Keine',
+        vehicle_occupied = 'Sie können dieses Fahrzeug nicht speichern, da es nicht leer ist',
+        vehicle_not_tracked = 'Fahrzeug konnte nicht verfolgt werden',
+        no_spawn = 'Bereich zu überfüllt'
     },
     success = {
-        vehicle_parked = 'Vehicle Stored',
-        vehicle_tracked = 'Vehicle Tracked',
+        vehicle_parked = 'Fahrzeug gespeichert',
+        vehicle_tracked = 'Fahrzeug verfolgt',
     },
     status = {
-        out = 'Out',
-        garaged = 'Garaged',
-        impound = 'Impounded By Police',
-        house = 'House',
+        out = 'Außen',
+        garaged = 'In Garage',
+        impound = 'Von der Polizei eingezogen',
+        house = 'Haus',
     },
     info = {
         car_e = 'Garage',
-        sea_e = 'Boathouse',
+        sea_e = 'Bootshaus',
         air_e = 'Hangar',
-        rig_e = 'Rig Lot',
+        rig_e = 'Bohrplatz',
         depot_e = 'Depot',
-        house_garage = 'House Garage',
+        house_garage = 'Hausgarage',
     }
 }
 
