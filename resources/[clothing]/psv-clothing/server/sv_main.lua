8e39808a321f3dde46816f56a311ee4851daf231
02Blacksnakes03 <82246205+02Blacksnakes03@users.noreply.github.com>
2024-09-28 23:55:30 +0200

    merge

--- resources/[clothing]/psv-clothing/server/sv_main.lua
+++
@@ -1,8 +0,0 @@
-local CodeID = {
-    codeName = "PSV - Clothing System 1.0",
-    version = "2.1.0"
-  }
-  
-  Citizen.CreateThread(function()
-  print('[' .. CodeID.codeName .. '] v' .. CodeID.version .. ' started!')
-  end)
